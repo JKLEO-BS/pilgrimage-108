24a8cffad10ee343b264f1ccf2cc73c5a46486bf
JKLEO-BS <jklee1116@gmail.com>
2026-04-17 08:57:24 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import KakaoCallback from "./pages/KakaoCallback.jsx";
 import "./index.css";
 import "./pwa.css";
 
+const path = window.location.pathname;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {path === "/auth/kakao/callback" ? <KakaoCallback /> : <App />}
   </React.StrictMode>
 );
